7a6abe2a4acb1f5095905462bd4a354f5f316250
Thiago Vasconcelos <120605650+vasconcelos-thiago@users.noreply.github.com>
2023-05-16 00:13:43 -0300

    Create pokeApi.js

---
+++ pokeApi.js
@@ -0,0 +1,36 @@
+const pokeApi = {}
+
+const convertPokeApiDetail =(pokeDetail) => {
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetail)
+}
+pokeApi.getPokemons = (offset, limit) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonDetails) => pokemonDetails)
+        
+}
